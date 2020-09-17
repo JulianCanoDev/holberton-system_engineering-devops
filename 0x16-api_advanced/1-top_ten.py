c77c6050f92dfe4d79e0add9c3bfa2b3e2b77ba8
julian <juliancano.dev@gmail.com>
2020-09-17 15:01:12 -0500

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""prints the titles of the first 10 hot"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    URL = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    headers = {'User-agent': 'URL'}
+    req = requests.get(URL, headers=headers, allow_redirects=False).json()
+    try:
+        lists = req.get('data').get('children')
+        for i in lists:
+            print(i.get('data').get('title'))
+    except:
+        print(None)
