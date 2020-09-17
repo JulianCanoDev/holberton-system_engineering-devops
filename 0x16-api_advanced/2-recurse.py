b67bb017c16cf063502c741a3b37cfe5a6955ac4
julian <juliancano.dev@gmail.com>
2020-09-17 15:05:47 -0500

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Re used"""
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    URL = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(
+        subreddit, after)
+    headers = {'User-agent': 'URL'}
+    req = requests.get(URL, headers=headers, allow_redirects=False).json()
+    try:
+        lists = req.get('data').get('children')
+        for i in lists:
+            hot_list.append(i.get('data').get('title'))
+    except:
+        return(None)
+
+    after_data = req.get('data').get('after')
+    if after_data is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after_data)
