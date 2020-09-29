884b885b804ef1a0d03bd70eb7e06d09c19468b8
julian <juliancano.dev@gmail.com>
2020-09-29 12:47:59 -0500

    Solved

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Script that applies fix to Apache server returning a 500 error
+exec { 'fix-apache':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+  }
